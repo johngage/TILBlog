a9f2b489e0e9726a6e321e3e1549bb04fd894669
johngage <john.gage@gmail.com>
2025-05-20 11:20:21 -0700

    fix netlify deployment problems

--- netlify/functions/api.py
+++ netlify/functions/api.py
@@ -1,12 +1,12 @@
 from flask import Flask
 import sys, os
-# Import serverless adapter
+# Import serverless adapter 
 from serverless_wsgi import handle_request
 
-# Add the parent directory to sys.path
+# Add parent directory to path
 sys.path.insert(0, os.path.dirname(os.path.dirname(os.path.dirname(__file__))))
 
-# Import your Flask app
+# Import your app
 from app import app
 
 def handler(event, context):
